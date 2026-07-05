52add2a77862f312d8f895c80668cfc3122945c4
KaloyanJk <kaloyan112@gmail.com>
2026-07-05 19:58:38 +0100

    Fix var

--- DIYMate/settings.py
+++ DIYMate/settings.py
@@ -200,14 +200,12 @@ ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_EMAIL_VERIFICATION = 'optional'
 ACCOUNT_LOGOUT_ON_GET = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = "SENDGRID_API_KEY"
 
 # CLOUDINARY_STORAGE = {
 #     'CLOUD_NAME': 'your_cloud_name',
