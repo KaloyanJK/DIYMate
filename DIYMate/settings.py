fe6387670f2991b0f969fe565a709581ce46ce5b
KaloyanJk <kaloyan112@gmail.com>
2026-07-05 18:52:46 +0100

    Fix var

--- DIYMate/settings.py
+++ DIYMate/settings.py
@@ -200,6 +200,15 @@ ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_EMAIL_VERIFICATION = 'optional'
 ACCOUNT_LOGOUT_ON_GET = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
 # CLOUDINARY_STORAGE = {
 #     'CLOUD_NAME': 'your_cloud_name',
 #     'API_KEY': 'your_api_key',
